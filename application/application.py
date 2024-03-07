081c25687b14ef288327f0817c71eacc2642934f
vince@CFS <vincentfschuang@gmail.com>
2024-03-07 19:50:51 +0800

    #to do: improve the accuracy of model

---
+++ application/application.py
@@ -0,0 +1,110 @@
+import dlib
+import cv2
+import torch
+import torchvision.transforms.functional as TF
+from imutils import face_utils, resize
+from moviepy.editor import VideoFileClip
+from tqdm.auto import tqdm
+from matplotlib import pyplot as plt
+from sklearn import preprocessing
+
+
+import sys
+import os
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
+from model.model import XceptionNet
+
+model = XceptionNet().cuda()
+state_dict = torch.load("model.pt")
+model.load_state_dict(torch.load('model.pt', map_location='cpu'))
+
+def preprocess_image(image):
+    image = TF.to_pil_image(image)
+    image = TF.resize(image, (128, 128))
+    image = TF.to_tensor(image)
+    image = (image - image.min())/(image.max() - image.min())
+    image = (2 * image) - 1
+    print(image.size())
+    return image.unsqueeze(0)
+
+def draw_landmarks_on_faces(image, faces_landmarks):
+    image = image.copy()
+    for landmarks, (left, top, height, width) in faces_landmarks:
+        landmarks = landmarks.view(-1, 2)
+        landmarks = (landmarks + 0.5)
+        landmarks[:, 0] = (landmarks[:, 0] - landmarks[:, 0].min()) / (landmarks[:, 0].max() - landmarks[:, 0].min())
+        landmarks[:, 1] = (landmarks[:, 1] - landmarks[:, 1].min()) / (landmarks[:, 1].max() - landmarks[:, 1].min())
+        landmarks = landmarks.numpy()
+        
+        for i, (x, y) in enumerate(landmarks, 1):
+            try:
+                cv2.circle(image, (int((x * width) + left), int((y * height) + top)), 2, [40, 117, 255], -1)
+            except:
+                pass
+        #plt.imshow(image)
+        #plt.show()
+    
+    return image
+
+face_detector = dlib.get_frontal_face_detector()
+
+@torch.no_grad()
+def inference(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    faces = face_detector(gray, 1)
+
+    outputs = []
+
+    for (i, face) in enumerate(faces):
+        (x, y, w, h) = face_utils.rect_to_bb(face)
+
+        crop_img = gray[y: y + h, x: x + w]
+        preprocessed_image = preprocess_image(crop_img)
+        landmarks_predictions = model(preprocessed_image.cuda())
+        outputs.append((landmarks_predictions.cpu(), (x, y, h, w)))
+
+    return draw_landmarks_on_faces(frame, outputs)
+
+
+def output_video(video, name, seconds = None):
+    total = int(video.fps * seconds) if seconds else int(video.fps * video.duration)
+    print('Will read', total, 'images...')
+    
+    outputs = []
+
+    writer = cv2.VideoWriter(name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), video.fps, tuple(video.size))
+
+    for i, frame in enumerate(tqdm(video.iter_frames(), total = total), 1):    
+        if seconds:
+            if (i + 1) == total:
+                break
+                
+        output = inference(frame)
+        outputs.append(output)
+
+        writer.write(cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+
+    writer.release()
+
+    return outputs
+
+if __name__ == '__main__':
+    video = VideoFileClip("application/video/meme3.mp4")
+    print('FPS: ', video.fps)
+    print('Duration: ', video.duration, 'seconds')
+
+    for frame in video.iter_frames():
+        break
+
+    
+    outputs = output_video(video, "video_output/Meme3 Face Detection")
+    plt.figure(figsize = (11, 11))
+    plt.imshow(outputs[10])
+
+    plt.show()
+
+
